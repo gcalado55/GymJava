7284713574aae2f108b5858b53e4c0dfa8812d24
gcalado55 <gcalado501@gmail.com>
2026-08-17 16:52:13 -0300

    feat: adiciona entidade Exercicio

---
+++ GymJava/api/src/main/java/com/treinoapp/api/model/Exercicio.java
@@ -0,0 +1,29 @@
+package com.treinoapp.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "exercicio")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String grupoMuscular;
+
+}
