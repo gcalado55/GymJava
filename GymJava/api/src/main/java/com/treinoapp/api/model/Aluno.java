100987c0194bc74d48ecf8853e2be59d6e73d8d6
gcalado55 <gcalado501@gmail.com>
2026-08-17 16:22:22 -0300

    feat: estrutura inicial do projeto e entidade Aluno

---
+++ GymJava/api/src/main/java/com/treinoapp/api/model/Aluno.java
@@ -0,0 +1,28 @@
+package com.treinoapp.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "aluno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
