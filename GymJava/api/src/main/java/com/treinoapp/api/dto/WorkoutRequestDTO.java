dc8c53454939211f0758d89112accecef4747922
gcalado55 <gcalado501@gmail.com>
2026-08-27 18:51:31 -0300

    refactor: adiciona @NotNull em memberId

--- GymJava/api/src/main/java/com/treinoapp/api/dto/WorkoutRequestDTO.java
+++ GymJava/api/src/main/java/com/treinoapp/api/dto/WorkoutRequestDTO.java
@@ -1,11 +1,12 @@
 package com.treinoapp.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record WorkoutRequestDTO(
         @NotBlank(message = "name is required") String name,
-        @NotBlank(message = "memberId is required") UUID memberId
+        @NotNull(message = "memberId is required") UUID memberId
     ){
 }
